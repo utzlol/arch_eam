5631dddc957fcbc786ff1f9eb50e32c9bf03c909
utzlol <40377777+utzlol@users.noreply.github.com>
2021-08-11 09:49:54 +0700

    Update eam.py

--- eam.py
+++ eam.py
@@ -1,14 +1,19 @@
-import requests
+import requests, subprocess
 
 a = requests.get('https://archlinux.org/mirrorlist/?country=all&protocol=http&protocol=https&ip_version=4')
 print(a.text)
 a = a.text
 
+username = str(subprocess.check_output('whoami'))
+username = username.replace("b'",'').replace("\\n'",'')
+dir = f'/home/{username}/mirrorlist'
+
 a = a.replace('#S', 'S')
 
-filew = open('mirrorlist', 'w')
+filew = open(dir, 'w')
 filew.write(a)
 filew.close()
 
-filer = open('mirrorlist', 'r')
+filer = open(dir, 'r')
 print(filer.read())
+subprocess.call(['sudo','mv',dir,'/etc/pacman.d/'])
